1b3f5b9a6ac19af4c546b50cf007b0f8754e71b4
placenameday <placenameday@gmail.com>
2022-04-27 15:15:24 +0800

    批量anova带正态检验与齐性检验

--- src/static_anova.R
+++ src/static_anova.R
@@ -1,19 +1,33 @@
 dta <- read_csv("/Users/placenameday/R study/edf_demo/data/processed/eye_tacking/tidy/eye_event_overall/inner_dt_stim.csv")
 
-dtb <- dta %>% mutate(stim=if_else(block_name=="b1b"&stim==1.5,1,stim)) %>%
-  filter(block_name!="b2") %>% select(-AOI)
+
+cl <- readxl::read_xlsx("/Users/placenameday/R study/edf_demo/para/para_data/check_list.xlsx") %>%
+  filter(eb==4)
+
+b4phone <- cl$phone
+
+
+dtb <- dta %>% mutate(stim=if_else(block_name=="b1b"&stim==1.5,1,stim), block_name=if_else(block_name=="b4a"&participant_phone %in% b4phone,"nb4b",if_else(block_name=="b4b"&participant_phone %in% b4phone,"nb4a",block_name))) %>% mutate(block_name=if_else(block_name=="nb4b", "b4b", if_else(block_name=="nb4a", "b4a", block_name))) %>%
+  select(-AOI)
 
 anv <- function(metri, stname, bname, dt3){
   mm <- metri
   b1a <- dt3 %>% filter(block_name==bname, stim==stname) %>%
-    group_by(maas_g, tgjl_g, participant_phone) %>%
+    group_by(maas_g, tgjl_g, sas_g, participant_phone) %>%
     summarise(fc=mean(!!sym(metri), na.rm=T)) %>%
-    mutate(maas = factor(maas_g, levels=c("high", "low")),
-           tgjl = factor(tgjl_g, levels=c("high", "low")))
+    mutate(maas = factor(maas_g, levels=c("high", "mid", "low")),
+           tgjl = factor(tgjl_g, levels=c("high", "mid", "low")),
+           sas = factor(sas_g, levels=c("high", "mid", "low")))
+  
+  ztjy <- round(shapiro.test(b1a$fc)$p.value,4)
+  
+  bbc <- b1a %>% pivot_longer(c(maas,tgjl,sas), names_to = "sclae", values_to = "group") %>% mutate(zg=paste(sclae,group,sep = "_"))
+  
+  qxjy <- round(ifelse(ztjy<0.05, leveneTest(bbc$fc,as.factor(bbc$zg))$`Pr(>F)`[1], bartlett.test(bbc$fc,bbc$zg)$p.value),4)
 
-  aov.manu <- try(aov(fc ~ maas + tgjl + maas:tgjl, data=b1a))
-  summary(aov.manu)[[1]] %>% mutate(block_name=bname, stim=stname, metric=mm, effect=rownames(.)) %>%
-    rename(p = "Pr(>F)")
+  aov.manu <- try(aov(fc ~ maas + tgjl + sas + maas:tgjl:sas + maas:tgjl + tgjl:sas + maas:sas, data=b1a))
+  summary(aov.manu)[[1]] %>% mutate(block_name=bname, stim=stname, metric=mm, effect=rownames(.), ztx=ztjy, qx=qxjy) %>%
+    rename(p = "Pr(>F)") %>% mutate(p=round(p,4))
 }
 
 # anv("fc","1", "b1a", dtb)
@@ -44,6 +58,21 @@ write_excel_csv(kankan, "/Users/placenameday/R study/edf_demo/data/processed/eye
 
 
 metrilist <- colnames(dtb)[7:19]
+
+
+bba <- dtb %>% filter(block_name=="b1a", stim==1) %>%
+  group_by(maas_g, tgjl_g, sas_g, participant_phone) %>%
+  summarise(fc=mean(fc, na.rm=T)) %>%
+  mutate(maas = factor(maas_g, levels=c("high", "mid", "low")),
+         tgjl = factor(tgjl_g, levels=c("high", "mid", "low")),
+         sas = factor(sas_g, levels=c("high", "mid", "low")))
+
+tt <- bba$fc
+
+bbc <- bba %>% pivot_longer(c(maas, tgjl, sas), names_to = "sclae", values_to = "group") %>% mutate(zg=paste(sclae,group,sep = "_"))
+a <- shapiro.test(bba$fc^2)
+b <- bartlett.test(bbc$fc,bbc$zg)
+c <- leveneTest(bbc$fc,as.factor(bbc$zg))
 
 
 ggplot(data=b1a, mapping = aes(
