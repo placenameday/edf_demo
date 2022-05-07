57c09be87f7a827ac92873f4955d5dde3cb9f2f9
placenameday <placenameday@gmail.com>
2022-05-07 21:56:32 +0800

    微眼跳探测

--- src/microsacc_detect.R
+++ src/microsacc_detect.R
@@ -5,7 +5,7 @@ SAMPLING = 500
 MINDUR = 3
 VFAC = 5
 
-
+library(data.table)
 
 compute_ms <- function(data){
   # data import
@@ -14,17 +14,14 @@ compute_ms <- function(data){
   ms <- microsacc(dt,VFAC,MINDUR,SAMPLING)
 }
 
-dt <- fread("/Users/placenameday/R study/edf_demo/data/processed/eye_tacking/tidy/raw_clean/1004_raw.csv") %>%
+dt <- fread("/Users/placenameday/R study/edf_demo/data/processed/eye_tacking/tidy/raw_clean/1003_raw.zip") %>%
   filter(stim==1, block_name=="b1a", trial_id==1, !is.na(xp), !is.na(yp)) %>% mutate(xp=xp/32, yp=yp/32) %>%
   select(xp, yp)
 
-dt2 <- fread("/Users/placenameday/R study/edf_demo/data/processed/eye_tacking/tidy/raw_clean/1022_raw.csv") %>%
-  filter(stim%in%c(1,2), block_name=="b2", trial_id==18, !is.na(xp), !is.na(yp)) %>% mutate(xp=xp/32, yp=yp/32) %>%
-  select(xp, yp)
-k2 <- compute_ms(dt2)
-k2[[1]]
 
 k <- compute_ms(dt)
-
-
+f <- as.data.frame(k[[1]]) %>% select(1:3) %>%
+  mutate(participant_phone=1, participant_group="a",block_name="b1a", stim=1, trial_id=1) %>%
+  rename(mxp=V1, myp=V2, mpv=V3)
+f
 
